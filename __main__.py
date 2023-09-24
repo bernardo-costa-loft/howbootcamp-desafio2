b0c8855379b24c132d57e3f96b4d9890ff541000
BERNARDO COSTA <bernardo.costa@loft.com.br>
2023-09-24 10:47:07 -0300

    clean code main

--- __main__.py
+++ __main__.py
@@ -1,4 +1,8 @@
-from utils.infra import create_data_lake_buckets
+from utils.infra import create_data_lake_buckets, create_emr_spark_cluster
+
+def create_infrastructure():
+    create_data_lake_buckets()
+    create_emr_spark_cluster()
 
 if __name__ == "__main__":
-    create_data_lake_buckets()
+    create_infrastructure()
